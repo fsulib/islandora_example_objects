cb5e00ac98a9ee99fca3616328c27b63ba5aa431
Favenzio Calvo <fcalvo@fsu.edu>
2017-11-03 16:03:07 +0000

    Recipe now runs using configuration settings.

--- src/Form/IslandoraExampleObjectsConfigForm.php
+++ src/Form/IslandoraExampleObjectsConfigForm.php
@@ -23,26 +23,22 @@ class IslandoraExampleObjectsConfigForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildForm($form, $form_state);
     $config = $this->config('islandora_example_objects.settings');
-
-    $node_types = \Drupal\node\Entity\NodeType::loadMultiple();
-    $node_type_titles = array();
-    foreach ($node_types as $machine_name => $val) {
-      $node_type_titles[$machine_name] = $val->label();
-    }
-
+    $node_type_titles = $this->getAllContentTypes();
+    
+    // Cycle through node types, set default values and add to form. 
     foreach ($node_type_titles as $node_type_machine_name => $node_type_title) {
+      // Retrieve value from module settings.
+      $current_node_type_number = $config->get($node_type_machine_name);
+      $default_number = ($current_node_type_number ? $current_node_type_number : 1);
+      
+      // Add the field to the form.
       $form[$node_type_machine_name] = array (
         '#type' => 'number',
         '#title' => $node_type_title,
+        '#default_value' => $default_number,
       );
     }
-
-    $form['node_types'] = array(
-      '#type' => 'checkboxes',
-      '#title' => $this->t('Node Types'),
-      '#options' => $node_type_titles,
-      '#default_value' => $config->get('islandora_example_objects.node_types'),
-    );
+    
     return $form;
   }
 
@@ -51,12 +47,18 @@ class IslandoraExampleObjectsConfigForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $config = $this->config('islandora_example_objects.settings');
-    $config->set('islandora_example_objects.node_types', $form_state->getValue('node_types'));
+    $node_type_titles = $this->getAllContentTypes();
+    
+    // Cycle through node types, set default values and add to form. 
+    foreach ($node_type_titles as $node_type_machine_name => $node_type_title) {    
+     $config->set($node_type_machine_name, $form_state->getValue($node_type_machine_name));
+    }
+    
     $config->save();
-
+    
     // Create the dummy content
     realistic_dummy_content_api_apply_recipe(new RealisticDummyContentWatchdogLog());
-
+    
     return parent::submitForm($form, $form_state);
   }
 
@@ -67,5 +69,18 @@ class IslandoraExampleObjectsConfigForm extends ConfigFormBase {
     return [
       'islandora_example_objects.settings',
     ];
+  }
+  
+  /**
+   * @return array of note types (key=machine_name, value=Node Label) 
+   */
+  protected function getAllContentTypes () {
+    $node_types = \Drupal\node\Entity\NodeType::loadMultiple();
+    $node_type_titles = array();
+    foreach ($node_types as $machine_name => $val) {
+      $node_type_titles[$machine_name] = $val->label();
+    }
+    
+    return $node_type_titles;
   }
 }
